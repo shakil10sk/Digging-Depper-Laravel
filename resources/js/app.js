8e360e087044efa97bb5d16a5a4a2cd868b1a02e
iitgituser <gituseriit@innovationit.com.bd>
2022-04-10 15:50:27 +0600

    laravel mix with vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
+import * as Vue from 'vue';
 require('./bootstrap');
+
+new Vue({
+    el: '#app',
+    created() {
+        alert('Enter into vue');
+        document.write("shakil");
+    }
+   
+})
+

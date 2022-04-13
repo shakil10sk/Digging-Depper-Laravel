fb6a9b12294c0c8fdb04cbbae08200cf64b4753d
iitgituser <gituseriit@innovationit.com.bd>
2022-04-13 15:51:32 +0600

    vue implement

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
+import Vue from 'Vue';
+import counter from './components/counter.vue';
+
 require('./bootstrap');
+
+// Vue.component('counter','./components/counter.vue')
+
+
+new Vue({
+        el:'#app',
+        counter
+            
+    })

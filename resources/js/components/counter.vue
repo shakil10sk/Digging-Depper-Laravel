187db00b0b0db80a081c3b937d1ef0b7cf01eab8
iitgituser <gituseriit@innovationit.com.bd>
2022-04-13 15:51:54 +0600

    vue implement

---
+++ resources/js/components/counter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="counter-area">
+        <h2>in vue</h2>
+        <button>-</button>
+        0
+        <button>+</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'Counter',
+}
+</script>
